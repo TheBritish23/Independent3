032cf55734d49f788c3fd9285cba12508b1e21db
Soro Cat <sorousha326@gmail.com>
2019-07-30 13:35:50 -0700

    add changes in js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
+var bb = function(){
+
+
 // user interface logic
 $(document).ready(function() {
   $("form#BB").submit(function(event) {
     event.preventDefault()
+    var words = $('#words').val()
+    var result = bb(words)
+    $("#result").text()
+  })
+ });
